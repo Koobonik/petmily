77aee44f9f4ce94562b6a4efb4866a48cac5f68d
devkoo <kbi960130@naver.com>
2021-01-25 17:19:31 +0900

    [IMP]: cors 추가

--- src/main/java/com/spring/configuration/web/WebConfig.java
+++ src/main/java/com/spring/configuration/web/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8084")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8084", "http:localhost:8080")
                 .allowCredentials(true)
                 //.maxAge(3600)
         ;
